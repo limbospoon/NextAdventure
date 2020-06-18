b38d41e113ed0688cb4d535e3387e6744bb258ff
John McCormack <silentsammy777@hotmail.com>
2020-06-18 15:39:46 +0100

    initial commit

---
+++ Assets/Scripts/Door/Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public void OpenDoor()
+    {
+
+    }
+
+    public void CloseDoor()
+    {
+
+    }
+}
